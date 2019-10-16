ff7c0e5c8a3af7211494342a2504cd6cb5f5dcfb
firasest <azizba015@gmail.com>
2019-10-16 11:34:35 +0100

    commit

--- app/src/main/java/com/firasjlassi/customlistview/MainActivity.java
+++ app/src/main/java/com/firasjlassi/customlistview/MainActivity.java
@@ -3,12 +3,29 @@ package com.firasjlassi.customlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected ListView maListViewPerso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        maListViewPerso =findViewById(R.id.listviewperso);
+
+        ArrayList <HashMap <String, String> > ListView = new ArrayList<>();
+
+        HashMap <String , String> map;
+
+        map =new HashMap<>();
+
+        map.put("titre","word");
+        map.put("img",String.valueOf(R.drawable.word));
+        ListView.add(map);
     }
 }
